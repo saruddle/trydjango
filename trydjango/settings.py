f3d9eccc2f1fba62683b7919bcd45ae10acf5b8d
Scott Ruddle <saruddle@hotmail.com>
2018-08-13 16:20:41 -0400

    Database Testing

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -14,6 +14,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#anotherDir = os.path.dirname(os.path.abspath(__file__))
+#thirdDir = os.path.abspath(__file__)
+#print(BASE_DIR)
+#print(anotherDir)
+#print(thirdDir)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my own apps
+    'products',
 ]
 
 MIDDLEWARE = [
