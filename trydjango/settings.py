1f1bc267a712a08db6bd06471fa56a9ca961e473
Scott Ruddle <saruddle@hotmail.com>
2018-08-15 23:39:32 -0400

    Took out django_heroku

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# Suggested by Heroku
-import django_heroku
+## Suggested by Heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +31,7 @@ DEBUG = False
 
 
 # The 'local' might work for local runserver but if not use [] for ALLOWED_HOSTS
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['great-american.herokuapp.com']
 
 
 # Application definition
@@ -154,4 +154,4 @@ EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 
 # Suggested by Heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
