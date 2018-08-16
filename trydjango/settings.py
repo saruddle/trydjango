fbca0a82a6a37d16661c1a1a2f601e5fcb57f8e5
Scott Ruddle <saruddle@hotmail.com>
2018-08-16 12:57:29 -0400

    testing django_heroku

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 # Suggested by Heroku
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,4 +162,4 @@ EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
 
 # Suggested by Heroku
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
